366374ccaf9c67be02a4ce7160211c08b0d1c4ca
Alberto Zapata <azapata@initiumsoft.com>
2020-05-03 18:11:45 -0500

    professional summary data was integrated, employment histrory datas was created

--- getData.py
+++ getData.py
@@ -4,8 +4,8 @@ import json
 class Data:
 
     @staticmethod
-    def get_data():
-        file = open('data/info.json', 'r')
+    def get_data(path):
+        file = open(path, 'r')
         data = json.load(file)
         file.close()
 
--- main.py
+++ main.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     d = Data()
-    info = d.get_data()
-    return render_template("index.html", info=info)
+    info = d.get_data('data/info.json')
+    summary = d.get_data('data/sections/summary.json')
+    return render_template("index.html", info=info, summary=summary)
 
 app.run(debug=True)
