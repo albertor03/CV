3e0887a764888ffc853454920796279f17f7787e
Alberto Zapata <azapata@initiumsoft.com>
2020-05-04 13:56:23 -0500

    skills data was implemented into skill section

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ def index():
     summary = d.get_data('data/sections/summary.json')
     employment = d.get_data('data/sections/employment.json')
     education = d.get_data('data/sections/education.json')
-    return render_template("index.html", info=info, summary=summary, employment=employment, education=education)
+    skill = d.get_data('data/sections/skills.json')
+    return render_template("index.html", info=info, summary=summary, employment=employment, education=education,
+                           skill=skill)
 
 
 app.run(debug=True)
