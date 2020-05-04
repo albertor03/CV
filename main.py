537799d196380b9e6e5a7c9010d4801047abf954
Alberto Zapata <azapata@initiumsoft.com>
2020-05-04 13:31:19 -0500

    employment data was implemented into employment section

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from getData import Data
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     d = Data()
     info = d.get_data('data/info.json')
     summary = d.get_data('data/sections/summary.json')
-    return render_template("index.html", info=info, summary=summary)
+    employment = d.get_data('data/sections/employment.json')
+    return render_template("index.html", info=info, summary=summary, employment=employment)
+
 
 app.run(debug=True)
