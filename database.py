8bad9d48c2c4235a434cb7d8c9927d555509f85d
alberto <azapata@initiumsoft.com>
2020-05-08 17:47:41 -0500

    Firebase was added into project

--- database.py
+++ database.py
@@ -1,9 +1,16 @@
-import firebase_admin
-from firebase_admin import credentials
-import requests
+import pyrebase
 
-cred = credentials.Certificate("database/cv-flask-firebase-adminsdk-hh9s9-f13a390c3e.json")
-firebase_admin.initialize_app(cred)
+firebaseConfig = {
+    "apiKey": "AIzaSyCHfMchjhqW_m9ARFGuxdzE2b0pABi1Bps",
+    "authDomain": "cv-flask.firebaseapp.com",
+    "databaseURL": "https://cv-flask.firebaseio.com",
+    "projectId": "cv-flask",
+    "storageBucket": "cv-flask.appspot.com",
+    "messagingSenderId": "594310230679",
+    "appId": "1:594310230679:web:3eebc3f3fa3ff7a7b5efd2",
+    "measurementId": "G-E2FZJVPSFE",
+    "serviceAccount": "database/cv-flask-firebase-adminsdk-hh9s9-f13a390c3e.json"
+}
 
-x = requests.get('https://cv-flask.firebaseio.com/course.json').text
-print(x)
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
